f837e4900a8266e6d8a2eaa6638ecd5372531b47
Nhala <zirconiump42@gmail.com>
2021-02-19 09:10:51 +0900

    ローディング中コンポーネントを作成

---
+++ resources/js/components/Loading.vue
@@ -0,0 +1,16 @@
+<!--ローディング中や検索中に表示するコンポーネント-->
+<template>
+  <div class="c-loading">
+    <span class="c-loading__circle c-loading__circle--1"></span>
+    <span class="c-loading__circle c-loading__circle--2"></span>
+    <span class="c-loading__circle c-loading__circle--3"></span>
+    <span class="c-loading__circle c-loading__circle--4"></span>
+    <span class="c-loading__circle c-loading__circle--5"></span>
+    <span class="c-loading__circle c-loading__circle--6"></span>
+    <span class="c-loading__circle c-loading__circle--7"></span>
+    <span class="c-loading__circle c-loading__circle--8"></span>
+    <span class="c-loading__circle c-loading__circle--9"></span>
+    <span class="c-loading__circle c-loading__circle--10"></span>
+  </div>
+</template>
+<script></script>
