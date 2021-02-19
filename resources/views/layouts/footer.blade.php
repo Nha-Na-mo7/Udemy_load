253568caadc804df25175e2eb60e8c2a9984b484
Nhala <zirconiump42@gmail.com>
2021-02-19 11:31:38 +0900

    フッター用のbladeテンプレートの作成と実装

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+{{--=========--}}
+{{-- フッター --}}
+{{--=========--}}
+<div class="p-footer">
+    <!-- Copyright -->
+    <div class="p-footer__copyright">
+        <span>
+            Copyright © UdemyLoad 2021
+            <br class="u-sp__block"/>
+            All Rights Reserved.
+        </span>
+    </div>
+</div>
