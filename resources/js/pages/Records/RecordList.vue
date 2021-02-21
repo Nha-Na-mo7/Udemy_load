b5224d5c16c5d63362c02adf6197fde561d7db55
Nhala <zirconiump42@gmail.com>
2021-02-21 19:26:47 +0900

    トップを視認しやすくなるように仮スタイルを付与

--- resources/js/pages/Records/RecordList.vue
+++ resources/js/pages/Records/RecordList.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Record List 学習の記録</h1>
+  <div class="f_page">
+    <h1>Record List 学習の記録</h1>
+  </div>
 </template>
+
+<style scoped>
+  .f_page {
+    font-size: 20px;
+    margin-top: 60px;
+    height: 320px;
+    background: #dffcf2;
+  }
+</style>
