d9fc29d39b74c60276e17f7549a9909a7b8b9019
Nhala <zirconiump42@gmail.com>
2021-02-24 11:27:36 +0900

    vue側でUdemyAPIを呼び出すメソッドを仮で実装

--- public/js/app.js
+++ public/js/app.js
@@ -2368,6 +2368,98 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/pages/Records/RecordList.vue?vue&type=script&lang=js&":
+/*!************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/pages/Records/RecordList.vue?vue&type=script&lang=js& ***!
+  \************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      searchWord: '',
+      isSearching: false,
+      searchData: {
+        keywords: ''
+      }
+    };
+  },
+  methods: {
+    searchCourse: function searchCourse() {
+      var _this = this;
+
+      return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee() {
+        var params, response;
+        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                if (!_this.isSearching) {
+                  _context.next = 2;
+                  break;
+                }
+
+                return _context.abrupt("return", false);
+
+              case 2:
+                console.log('search START!!!'); // 検索開始
+
+                _this.isSearching = true; // 検索ワードを元にUdemyAPIにリクエストする
+
+                params = _this.searchData;
+                _context.next = 7;
+                return axios.get('/udemy/course/get', {
+                  params: params
+                });
+
+              case 7:
+                response = _context.sent;
+                _this.isSearching = false;
+                console.log('メソッド finished!');
+
+              case 10:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      }))();
+    }
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/pages/Records/RecordList.vue?vue&type=style&index=0&id=4f175436&scoped=true&lang=css&":
 /*!*******************************************************************************************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/css-loader??ref--6-1!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--6-2!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/pages/Records/RecordList.vue?vue&type=style&index=0&id=4f175436&scoped=true&lang=css& ***!
@@ -21989,18 +22081,44 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
-}
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "f_page" }, [
-      _c("h1", [_vm._v("Record List 学習の記録")])
+  return _c("div", { staticClass: "f_page" }, [
+    _c("h1", [_vm._v("Record List 学習の記録")]),
+    _vm._v(" "),
+    _c("div", [
+      _c("h2", [_vm._v("UDEMY講座検索")]),
+      _vm._v(" "),
+      _c("form", { attrs: { action: "" } }, [
+        _c("label", [
+          _c("input", {
+            directives: [
+              {
+                name: "model",
+                rawName: "v-model",
+                value: _vm.searchData.keywords,
+                expression: "searchData.keywords"
+              }
+            ],
+            attrs: { type: "text" },
+            domProps: { value: _vm.searchData.keywords },
+            on: {
+              input: function($event) {
+                if ($event.target.composing) {
+                  return
+                }
+                _vm.$set(_vm.searchData, "keywords", $event.target.value)
+              }
+            }
+          })
+        ])
+      ]),
+      _vm._v(" "),
+      _c("button", { staticClass: "c-btn", on: { click: _vm.searchCourse } }, [
+        _vm._v("講座検索\n    ")
+      ])
     ])
-  }
-]
+  ])
+}
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -39045,17 +39163,19 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _RecordList_vue_vue_type_template_id_4f175436_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./RecordList.vue?vue&type=template&id=4f175436&scoped=true& */ "./resources/js/pages/Records/RecordList.vue?vue&type=template&id=4f175436&scoped=true&");
-/* harmony import */ var _RecordList_vue_vue_type_style_index_0_id_4f175436_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./RecordList.vue?vue&type=style&index=0&id=4f175436&scoped=true&lang=css& */ "./resources/js/pages/Records/RecordList.vue?vue&type=style&index=0&id=4f175436&scoped=true&lang=css&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _RecordList_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./RecordList.vue?vue&type=script&lang=js& */ "./resources/js/pages/Records/RecordList.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _RecordList_vue_vue_type_style_index_0_id_4f175436_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./RecordList.vue?vue&type=style&index=0&id=4f175436&scoped=true&lang=css& */ "./resources/js/pages/Records/RecordList.vue?vue&type=style&index=0&id=4f175436&scoped=true&lang=css&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
-  script,
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__["default"])(
+  _RecordList_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _RecordList_vue_vue_type_template_id_4f175436_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"],
   _RecordList_vue_vue_type_template_id_4f175436_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
@@ -39069,6 +39189,20 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
 if (false) { var api; }
 component.options.__file = "resources/js/pages/Records/RecordList.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/pages/Records/RecordList.vue?vue&type=script&lang=js&":
+/*!****************************************************************************!*\
+  !*** ./resources/js/pages/Records/RecordList.vue?vue&type=script&lang=js& ***!
+  \****************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_RecordList_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib??ref--4-0!../../../../node_modules/vue-loader/lib??vue-loader-options!./RecordList.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/pages/Records/RecordList.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_RecordList_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
--- resources/js/pages/Records/RecordList.vue
+++ resources/js/pages/Records/RecordList.vue
@@ -1,8 +1,53 @@
 <template>
   <div class="f_page">
     <h1>Record List 学習の記録</h1>
+
+    <div>
+      <h2>UDEMY講座検索</h2>
+      <form action="">
+        <label>
+          <input type="text" v-model="searchData.keywords">
+        </label></form>
+      <button
+          class="c-btn"
+          @click="searchCourse"
+      >講座検索
+      </button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchWord: '',
+      isSearching: false,
+      searchData: {
+        keywords: '',
+      }
+    }
+  },
+  methods: {
+    async searchCourse() {
+      // 検索中に重複して呼び出せないようにする
+      if ( this.isSearching ) {
+        return false;
+      }
+      console.log('search START!!!')
+      // 検索開始
+      this.isSearching = true;
+
+      // 検索ワードを元にUdemyAPIにリクエストする
+      const params = this.searchData;
+      const response = await axios.get('/udemy/course/get', { params });
+
+      this.isSearching = false;
+      console.log('メソッド finished!')
+    }
+  }
+}
+</script>
 
 <style scoped>
   .f_page {
