6231da19c8bead949ecbbf622e56f144e77286ff
Nhala <zirconiump42@gmail.com>
2021-03-08 15:12:39 +0900

    詳細画面へ追記

--- resources/js/pages/Records/RecordDetail.vue
+++ resources/js/pages/Records/RecordDetail.vue
@@ -1,13 +1,40 @@
 <template>
-$END$
+  <div>
+    <h2>レコードの詳細画面です</h2>
+  </div>
 </template>
-
 <script>
+
+import { OK } from '../../util.js'
+
 export default {
-name: "RecordDetail"
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      record: null,
+    }
+  },
+  methods: {
+    // レコードの情報をDBから取得する
+    async fetchRecord() {
+      console.log('レコード情報を取得しました。')
+    }
+  },
+  watch: {
+    $route: {
+      async handler() {
+        await this.fetchRecord()
+      },
+      immediate: true
+    }
+  }
 }
 </script>
-
 <style scoped>
 
 </style>
