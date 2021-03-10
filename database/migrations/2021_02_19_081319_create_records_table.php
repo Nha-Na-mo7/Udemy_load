fb3b78096dbcee989646d96ad01525511af97198
Nhala <zirconiump42@gmail.com>
2021-03-10 12:17:57 +0900

    レポートの削除フラグを追加

--- database/migrations/2021_02_19_081319_create_records_table.php
+++ database/migrations/2021_02_19_081319_create_records_table.php
@@ -18,6 +18,7 @@ class CreateRecordsTable extends Migration
             $table->unsignedBigInteger('user_id')->comment('投稿者ID');
             $table->string('title')->comment('投稿タイトル');
             $table->text('description')->comment('概要・説明');
+            $table->boolean('delete_flg')->comment('削除フラグ');
             $table->timestamps();
             
             // usersテーブルのidと紐付け
