b50c427255dc6b24468d913faa8e76161912a3d0
Nhala <zirconiump42@gmail.com>
2021-03-11 16:29:15 +0900

    インデントの修正

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -41,22 +41,21 @@ class RecordController extends Controller
         
         // coursesテーブルに選択されたコース、コースの説明、レコードの何番目かなどの情報を格納する
         for ($i = 0, $iMax = count($request->selectedCourses); $i < $iMax; $i++) {
-  
-          $course = new Course();
-          
-          $courseData = $request->get('selectedCourses')[$i];
-          $courseObj = $courseData['courseObject'];
-          
-          // contentsテーブルへ本文を格納
-          $course->record_id = $keep_id;
-          $course->record_index = $i;
-          $course->course_id = $courseObj['id'];
-          $course->title = $courseObj['title'];
-          $course->instructor = $courseObj['visible_instructors'][0]['title'];
-          $course->url = $courseObj['url'];
-          $course->image_url = $courseObj['image_240x135'];
-          $course->description = $courseData['description'];
-          $course->save();
+            $course = new Course();
+            
+            $courseData = $request->get('selectedCourses')[$i];
+            $courseObj = $courseData['courseObject'];
+            
+            // contentsテーブルへ本文を格納
+            $course->record_id = $keep_id;
+            $course->record_index = $i;
+            $course->course_id = $courseObj['id'];
+            $course->title = $courseObj['title'];
+            $course->instructor = $courseObj['visible_instructors'][0]['title'];
+            $course->url = $courseObj['url'];
+            $course->image_url = $courseObj['image_240x135'];
+            $course->description = $courseData['description'];
+            $course->save();
         }
         
         return response($record, 201);
@@ -64,37 +63,37 @@ class RecordController extends Controller
     
     // レコード詳細の取得
     public function show(string $id) {
-      Log::debug('==================================');
-      Log::debug('レコード詳細取得/ID:'.$id);
-      Log::debug('==================================');
-      // IDに合致するレコード情報を取得
-      $record = Record::where('id', $id)->with(['owner', 'courses'])->first();
-      
-      // TODO course内のindex項目による並び替えを行ってからreturnしてください
-      // レコードを返すが、存在しない場合は404を返す
-      return $record ?? abort(404);
+        Log::debug('==================================');
+        Log::debug('レコード詳細取得/ID:'.$id);
+        Log::debug('==================================');
+        // IDに合致するレコード情報を取得
+        $record = Record::where('id', $id)->with(['owner', 'courses'])->first();
+        
+        // TODO course内のindex項目による並び替えを行ってからreturnしてください
+        // レコードを返すが、存在しない場合は404を返す
+        return $record ?? abort(404);
     }
     
     // レコード一覧の取得
     public function index($user_id = null) {
-      Log::debug('==============');
-      Log::debug('レコード一覧取得');
-      Log::debug('==============');
-      // ユーザーIDがない場合、全てのレコードを取得する
-      if ($user_id === null) {
-        Log::debug('USER_ID:'.$user_id);
-        $records = Record::with(['owner'])
-            ->orderBy(Course::CREATED_AT, 'desc')
-            ->paginate();
-  
-        return $records;
-      }
-      // ユーザーIDがある場合、そのユーザーが投稿したレコードに絞って取得する
-      $records = Record::where('user_id', $user_id)
-          ->with(['owner'])
-          ->orderBy(Record::CREATED_AT, 'desc')
-          ->paginate();
+        Log::debug('==============');
+        Log::debug('レコード一覧取得');
+        Log::debug('==============');
+        // ユーザーIDがない場合、全てのレコードを取得する
+        if ($user_id === null) {
+            Log::debug('USER_ID:'.$user_id);
+            $records = Record::with(['owner'])
+                ->orderBy(Course::CREATED_AT, 'desc')
+                ->paginate();
       
-      return $records;
+            return $records;
+        }
+        // ユーザーIDがある場合、そのユーザーが投稿したレコードに絞って取得する
+        $records = Record::where('user_id', $user_id)
+            ->with(['owner'])
+            ->orderBy(Record::CREATED_AT, 'desc')
+            ->paginate();
+        
+        return $records;
     }
 }
