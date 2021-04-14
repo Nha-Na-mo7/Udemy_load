5d48d7c99438c4d2d8a16c7cabea202155d4098d
Nhala <zirconiump42@gmail.com>
2021-04-14 12:34:34 +0900

    ルーティングの並びを変更

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 // トップページ
 Route::get('/', 'HomeController@index')->name('home.index');
-
 // ログインユーザーを返す
 Route::get('/user', fn() => Auth::user())->name('user');
-
 // トークンリフレッシュ
 Route::get('/reflesh-token', function (Illuminate\Http\Request $request){
   $request->session()->regenerateToken();
@@ -31,6 +29,14 @@ Auth::routes();
 後で修正をすること */
 // ログイン
 Route::get('/login', 'HomeController@index')->name('home.index');
+
+// ===============
+// マイページ関連
+// ===============
+// 指定したユーザー情報の取得
+Route::get('/user/info/{username?}', 'UserController@get_user');
+// マイページのビュー
+Route::get('/mypage/{any?}', 'MypageController@index')->name('mypage.index')->where('any', '.+');
 
 // =============
 // レコード取得
@@ -45,9 +51,11 @@ Route::get('/records/new', 'RecordController@index_create')->name('record.index'
 // レコード詳細画面のビュー
 Route::get('/records/{any?}', 'RecordController@index')->name('record.index')->where('any', '.+');
 
-// =============
+// 以下は認証必須
+
+// ==============
 // レコード編集関連
-// =============
+// ==============
 // 投稿
 Route::post('/records/create', 'RecordController@create')->name('record.create');
 // 更新
@@ -57,18 +65,15 @@ Route::post('/record/{record}/comments', 'RecordController@addComment')->name('r
 // 削除(論理)
 Route::post('/record/{id}/delete', 'RecordController@delete')->name('record.delete');
 
-
-// =================
-// UdemyAPI関連
-// =================
+// ===============
+// UdemyAPI
+// ===============
 // 指定したワードでUdemyAPIを使用し、レッスン一覧を取得する
 Route::get('/udemy/course/get', 'UdemyController@get_course');
 
-// ====================
-// マイページ・ユーザー関連
-// ====================
-// 指定したユーザー情報の取得
-Route::get('/user/info/{username?}', 'UserController@get_user');
+// ===============
+// アカウント設定関連
+// ===============
 // ユーザーネームを更新する
 Route::post('/user/update/name', 'UserController@update_name');
 // メールアドレスの更新処理
@@ -82,8 +87,6 @@ Route::post('/user/update/password', 'UserController@update_password');
 // 退会処理
 Route::post('/withdraw', 'UserController@withdraw')->name('user.withdraw');
 
-// マイページのビュー
-Route::get('/mypage/{any?}', 'MypageController@index')->name('mypage.index')->where('any', '.+');
 // アカウント設定のビュー
 Route::get('/settings/{any?}', 'UserController@index')->name('settings.index')->where('any', '.+');
 
