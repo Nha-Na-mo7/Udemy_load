8771ef6e15f29106ff6c273b2088f04c06c5692f
Nhala <zirconiump42@gmail.com>
2021-05-11 10:49:03 +0900

    本番環境の時のみSSL化を有効にする記述

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,12 +22,18 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
+        // 開発環境の時、SQLをログに表示する
         if (config('app.env') !== 'production') {
           \DB::listen(function ($query) {
             \Log::info("Query Time:{$query->time}s] $query->sql");
           });
+        }
+        
+        // 本番環境の時、URLをhttpsにする
+        if (config('app.env') === 'production') {
+          $url->forceScheme('https');
         }
     }
 }
