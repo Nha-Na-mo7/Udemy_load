a5a3609581fef5ba94ed15c2a8db2d3bfc66c6b2
Nhala <zirconiump42@gmail.com>
2021-07-06 15:12:08 +0900

    カラム位置の調整

--- database/migrations/2021_07_06_145219_add_profiles_to_users.php
+++ database/migrations/2021_07_06_145219_add_profiles_to_users.php
@@ -14,8 +14,8 @@ class AddProfilesToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-          $table->string('organization')->nullable()->comment('所属企業・組織など');
-          $table->text('profile_text')->nullable()->comment('自己紹介');
+          $table->string('organization')->nullable()->after('remember_token')->comment('所属企業・組織など');
+          $table->text('profile_text')->nullable()->after('organization')->comment('自己紹介');
         });
     }
 
